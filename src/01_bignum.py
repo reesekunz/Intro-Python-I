ea07b7b3558d2f8135f33ba33efe89b1a254c38b
Reese Kunz <reesekunz21@gmail.com>
2019-12-02 14:54:53 -0700

    finished first 4 files

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,9 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+
+bignum = 2**65536
+print(bignum)
+
+# OR
+# print(2**65536)
