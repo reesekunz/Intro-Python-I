960e8886a1954c1d0b5d5dd609d1c0ffaa4c16da
Reese Kunz <reesekunz21@gmail.com>
2020-01-15 19:36:34 -0700

    finished first 5 files

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,33 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE 
+x = x + y
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE 
+x.insert(5, 99)  # at index 5, insert 99
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+mutiply_values = [number * 1000 for number in x]
+print(mutiply_values)
+
+# OR
+
+mutiply_values_two = []
+for number in x:
+    value = number * 1000
+    mutiply_values_two.append(value)
+
+print(mutiply_values_two)
